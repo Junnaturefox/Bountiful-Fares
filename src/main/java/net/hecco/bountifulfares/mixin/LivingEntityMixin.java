e82b737ae0e5adc11bf83ad819412a699e05cd6a
Hecco <126524389+Hecco56@users.noreply.github.com>
2025-02-14 14:06:45 -0600

    Fix acidic remove

--- src/main/java/net/hecco/bountifulfares/mixin/LivingEntityMixin.java
+++ src/main/java/net/hecco/bountifulfares/mixin/LivingEntityMixin.java
@@ -16,6 +16,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
@@ -88,10 +89,10 @@ public abstract class LivingEntityMixin {
         }
         return effect;
     }
-    @Inject(method = "removeStatusEffect", at = @At("HEAD"))
-    private void bountifulfares_acidicRemove(RegistryEntry<StatusEffect> effect, CallbackInfoReturnable<Boolean> cir) {
-        if (effect == BFEffects.ACIDIC && this.activeStatusEffects.containsKey(BFEffects.ACIDIC)) {
-            int acidicAmplifier = this.activeStatusEffects.get(effect).getAmplifier();
+    @Inject(method = "onStatusEffectRemoved", at = @At("HEAD"))
+    private void bountifulfares_acidicRemove(StatusEffectInstance effect, CallbackInfo ci) {
+        if (effect.getEffectType() == BFEffects.ACIDIC && !this.activeStatusEffects.containsKey(BFEffects.ACIDIC)) {
+            int acidicAmplifier = effect.getAmplifier();
             Iterator<Map.Entry<RegistryEntry<StatusEffect>, StatusEffectInstance>> iterator = this.activeStatusEffects.entrySet().iterator();
             ArrayList<StatusEffectInstance> newEffects = new ArrayList<>();
             while (iterator.hasNext()) {
